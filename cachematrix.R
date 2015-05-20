e73c51a53ffca0ae737a7cb47337eb63da0f36cc
Karen Tangco <karen.tangco@y7mail.com>
2015-05-20 13:42:26 +0800

    submitting Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function stores four functions:
+## 1 - set - used to reset the matrix, if need be
+## 2 - get - returns the input from the main function 'makeCacheMatrix'
+## 3 - setinv - stores the input variable inv into the main function 'makeCacheMatrix'
+## 4 - getinv - returns the variable i from the main function 'makeCacheMatrix'
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix) {
+        ## creating/initialising variable i
+        i <- NULL
+        
+        ## creating first function 'set', used to reset the matrix, if need be
+        set <- function(y = matrix()) {
+                x <<- y
+                i <<- NULL
+                return (x)
+        }
+        ## creating second function 'get', which just returns the variable x
+        ## from the main function 'makeCacheMatrix'
+        get <- function() x
+        
+        ## creating third function 'setinv', stores the input variable inv into
+        ## the main function 'makeCacheMatrix'
+        setinv <- function(inv) i <<- inv
+        
+        ## create fourth function 'getinv', which returns the variable i
+        getinv <- function() i
+        
+        ## store the above four functions in the main function 'makeCacheMatrix'
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
+## This function returns the inverse of the matrix x.  If the inverse has already
+## been calculated for a given matrix, function just retrieves from cache.  
+## Otherwise, it calculates and returns the inverse.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+        
+        i <- x$getinv()
+        
+        ## check to see if i already exists.  If so, retrieve from cache.
+        
+        if(!is.null(i)) {       
+                message("Retrieving cached data:")
+                return(i)
+        }       
+        
+        ## if the inveserse doesn't exist, then calculate it and return it
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinv(i)
+        i
 }
